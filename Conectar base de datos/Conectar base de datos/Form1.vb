c83b5d727ed055af668bd3277a1556d6a62114b4
Modingeroliver7 <43653831+Modingeroliver7@users.noreply.github.com>
2018-11-08 11:36:35 -0300

    Add files via upload

---
+++ Conectar base de datos/Conectar base de datos/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim MySqlCon As New MySqlConnection("datasource=localhost;port=3306;username=root;password='';database=sindelen_proy;SslMode=none")
+        Try
+
+            MySqlCon.Open()
+            MsgBox("Conexion Realizada")
+            MySqlCon.Close()
+        Catch ex As Exception
+            MsgBox("Conexion Faliida")
+            MySqlCon.Close()
+
+        End Try
+
+    End Sub
+End Class
